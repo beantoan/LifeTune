04b3e930764784cd78c7acf8676a8543c0c6aeee
Đậu Quốc Toản <beantoan@gmail.com>
2017-11-22 23:20:37 +0100

    Integrate with ExoPlayer for playing a playlist

--- app/src/main/java/it/unical/mat/lifetune/activity/MainActivity.kt
+++ app/src/main/java/it/unical/mat/lifetune/activity/MainActivity.kt
@@ -1,22 +1,42 @@
 package it.unical.mat.lifetune.activity
 
-import android.app.ActionBar
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.ViewPager
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.DynamicConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import it.unical.mat.lifetune.R
 import it.unical.mat.lifetune.adapter.AppSectionsPagerAdapter
 
 
-class MainActivity : AppCompatActivity(), android.support.v7.app.ActionBar.TabListener {
-    lateinit var mAppSectionsPagerAdapter: AppSectionsPagerAdapter
+class MainActivity : AppCompatActivity(), ActionBar.TabListener {
 
+    private lateinit var mAppSectionsPagerAdapter: AppSectionsPagerAdapter
+
+    private lateinit var mActionBar : ActionBar
+
+    @BindView(R.id.pager)
     lateinit var mViewPager: ViewPager
+
+    @BindView(R.id.music_player)
+    lateinit var mMusicPlayer: SimpleExoPlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,63 +45,73 @@ class MainActivity : AppCompatActivity(), android.support.v7.app.ActionBar.TabLi
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        super.onCreateOptionsMenu(menu)
+        super.onCreateOptionsMenu(menu)
 
         menuInflater.inflate(R.menu.options_menu, menu)
-//
-//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-//        val searchView = menu!!.findItem(R.id.action_search).actionView as SearchView
-//
-//        val cn = ComponentName(this, SearchMusicResultsActivity::class.java)
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(cn))
-//
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-//
-//        searchView.setIconifiedByDefault(false)
-//
-//        searchView.setOnQueryTextListener(object : OnQueryTextListener {
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                Log.d(TAG, "onQueryTextSubmit: $query")
-//                return true
-//            }
-//
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                Log.d(TAG, "onQueryTextSubmit: $newText")
-//                return true
-//            }
-//
-//        })
 
         return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        onDestroyTasks()
+    }
+
+    override fun onTabReselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+
+    }
+
+    override fun onTabUnselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+
+    }
+
+    override fun onTabSelected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+        mViewPager.currentItem = tab!!.position
     }
 
     private fun onCreateTasks() {
         setContentView(R.layout.activity_main)
 
+        ButterKnife.bind(this)
+
+        setupActionBar()
+
+        setupViewPager()
+
+        setupMusicPlayer()
+    }
+
+    private fun onDestroyTasks() {
+        mMusicPlayer.player.release()
+    }
+
+    private fun setupActionBar() {
+        // Set up the action bar.
+        mActionBar = supportActionBar!!
+
+        // Specify that the Home/Up button should not be enabled, since there is no hierarchical
+        // parent.
+        mActionBar.setHomeButtonEnabled(false)
+
+        // Specify that we will be displaying tabs in the action bar.
+        mActionBar.navigationMode = ActionBar.NAVIGATION_MODE_TABS
+    }
+
+    private fun setupViewPager() {
         // Create the adapter that will return a fragment for each of the three primary sections
         // of the app.
         mAppSectionsPagerAdapter = AppSectionsPagerAdapter(supportFragmentManager)
 
-        // Set up the action bar.
-        val actionBar = supportActionBar!!
-
-        // Specify that the Home/Up button should not be enabled, since there is no hierarchical
-        // parent.
-        actionBar.setHomeButtonEnabled(false)
-
-        // Specify that we will be displaying tabs in the action bar.
-        actionBar.navigationMode = ActionBar.NAVIGATION_MODE_TABS
-
         // Set up the ViewPager, attaching the adapter and setting up a listener for when the
         // user swipes between sections.
-        mViewPager = findViewById(R.id.pager)
         mViewPager.adapter = mAppSectionsPagerAdapter
         mViewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
             override fun onPageSelected(position: Int) {
                 // When swiping between different app sections, select the corresponding tab.
                 // We can also use ActionBar.Tab#select() to do this if we have a reference to the
                 // Tab.
-                actionBar.setSelectedNavigationItem(position)
+                mActionBar.setSelectedNavigationItem(position)
             }
         })
 
@@ -90,23 +120,53 @@ class MainActivity : AppCompatActivity(), android.support.v7.app.ActionBar.TabLi
             // Create a tab with text corresponding to the page title defined by the adapter.
             // Also specify this Activity object, which implements the TabListener interface, as the
             // listener for when this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
+            mActionBar.addTab(
+                    mActionBar.newTab()
                             .setText(mAppSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this))
         }
     }
 
-    override fun onTabReselected(tab: android.support.v7.app.ActionBar.Tab?, ft: FragmentTransaction?) {
+    private fun setupMusicPlayer() {
+
+        val bandwidthMeter = DefaultBandwidthMeter()
+        val videoTrackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
+        val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+
+        mMusicPlayer.player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+
+//        mMusicPlayer.player = ExoPlayerFactory.newSimpleInstance(
+//                DefaultRenderersFactory(this),
+//                DefaultTrackSelector(),
+//                DefaultLoadControl()
+//        )
+
+        playMusic(dummyMediaSources())
     }
 
-    override fun onTabUnselected(tab: android.support.v7.app.ActionBar.Tab?, ft: FragmentTransaction?) {
-
+    private fun playMusic(dynamicConcatenatingMediaSource: DynamicConcatenatingMediaSource) {
+        mMusicPlayer.player.prepare(dynamicConcatenatingMediaSource)
+        mMusicPlayer.player.playWhenReady = true
     }
 
-    override fun onTabSelected(tab: android.support.v7.app.ActionBar.Tab?, ft: FragmentTransaction?) {
-        mViewPager.currentItem = tab!!.position
+    private fun dummyMediaSources() : DynamicConcatenatingMediaSource {
+        val dynamicConcatenatingMediaSource = DynamicConcatenatingMediaSource()
+        val mediaSources = ArrayList<MediaSource>()
 
+        val songUrls = arrayListOf<String>(
+                "http://test.flanet.vn/1.mp3", "http://test.flanet.vn/2.mp3"
+        )
+
+        songUrls.forEach { mediaSources.add(buildMediaSource(Uri.parse(it))) }
+
+        dynamicConcatenatingMediaSource.addMediaSources(mediaSources)
+
+        return dynamicConcatenatingMediaSource
+    }
+
+    private fun buildMediaSource(uri: Uri) : MediaSource {
+      return ExtractorMediaSource(uri, DefaultHttpDataSourceFactory("ua"),
+              DefaultExtractorsFactory(), null, null)
     }
 
     companion object {
