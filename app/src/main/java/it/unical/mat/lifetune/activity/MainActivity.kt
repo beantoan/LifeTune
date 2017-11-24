e1cc286848fc195f66605169cd30f44a938059ed
Đậu Quốc Toản <beantoan@gmail.com>
2017-11-24 12:12:49 +0100

    Fixed problem with overlaying the suggestion results of search view

--- app/src/main/java/it/unical/mat/lifetune/activity/MainActivity.kt
+++ app/src/main/java/it/unical/mat/lifetune/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
@@ -29,7 +30,7 @@ import it.unical.mat.lifetune.data.ColorSuggestion
 import it.unical.mat.lifetune.data.DataHelper
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
     private lateinit var mAppSectionsPagerAdapter: AppSectionsPagerAdapter
 
     @BindView(R.id.tabs)
@@ -43,6 +44,9 @@ class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.floating_search_view)
     lateinit var mFloatingSearchView: FloatingSearchView
+
+    @BindView(R.id.app_bar_layout)
+    lateinit var mAppBarLayout: AppBarLayout
 
     private var mLastQuery = ""
 
@@ -60,6 +64,10 @@ class MainActivity : AppCompatActivity() {
         onDestroyTasks()
     }
 
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        mFloatingSearchView.translationY = verticalOffset.toFloat()
+    }
+
     private fun onCreateTasks() {
         setContentView(R.layout.activity_main)
 
@@ -68,6 +76,8 @@ class MainActivity : AppCompatActivity() {
         setupViewPager()
 
         setupMusicPlayer()
+
+        mAppBarLayout.addOnOffsetChangedListener(this)
     }
 
     private fun onDestroyTasks() {
