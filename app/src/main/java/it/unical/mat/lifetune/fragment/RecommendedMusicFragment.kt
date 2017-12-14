1bf8064a1a24768d8354ba1229e3e9cb2794e5f2
Đậu Quốc Toản <beantoan@gmail.com>
2017-12-14 17:01:35 +0100

    Created BaseMusicFragment

--- app/src/main/java/it/unical/mat/lifetune/fragment/RecommendedMusicFragment.kt
+++ app/src/main/java/it/unical/mat/lifetune/fragment/RecommendedMusicFragment.kt
@@ -1,7 +1,6 @@
 package it.unical.mat.lifetune.fragment
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -9,25 +8,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.thedeanda.lorem.LoremIpsum
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import it.unical.mat.lifetune.R
 import it.unical.mat.lifetune.controller.MusicController
 import it.unical.mat.lifetune.decoration.CategoryDividerItemDecoration
 import it.unical.mat.lifetune.entity.Category
-import it.unical.mat.lifetune.entity.Playlist
+import it.unical.mat.lifetune.service.ApiServiceFactory
+import it.unical.mat.lifetune.service.CategoryServiceInterface
+import it.unical.mat.lifetune.util.AppUtils
 import kotlinx.android.synthetic.main.fragment_recommended_music.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 /**
  * Created by beantoan on 11/17/17.
  */
-class RecommendedMusicFragment : Fragment(), MusicController.AdapterCallbacks {
-
+class RecommendedMusicFragment : BaseMusicFragment(), MusicController.AdapterCallbacks {
     lateinit var musicController: MusicController
-
-    private var categories: List<Category> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_recommended_music, container, false)
@@ -37,6 +34,12 @@ class RecommendedMusicFragment : Fragment(), MusicController.AdapterCallbacks {
         super.onViewCreated(view, savedInstanceState)
 
         onCreateViewTasks(view)
+    }
+
+    override fun onDestroy() {
+        mCompositeDisposable.clear()
+
+        super.onDestroy()
     }
 
     override fun onPlaylistClicked(category: Category?, position: Int) {
@@ -50,16 +53,16 @@ class RecommendedMusicFragment : Fragment(), MusicController.AdapterCallbacks {
 
         setupMusicController()
 
-        updateMusicController(dummyPlaylistData())
+        callCategoriesIndexService()
     }
 
     private fun setupRecyclerViewCategories() {
         Log.d(TAG, "setupRecyclerViewCategories")
 
-        val dividerDrawable = ContextCompat.getDrawable(context!!, R.drawable.category_divider)
-        val dividerItemDecoration = CategoryDividerItemDecoration(activity!!, DividerItemDecoration.VERTICAL, dividerDrawable!!)
+        val dividerDrawable = ContextCompat.getDrawable(activity!!.applicationContext, R.drawable.category_divider)
+        val dividerItemDecoration = CategoryDividerItemDecoration(activity!!.applicationContext, DividerItemDecoration.VERTICAL, dividerDrawable!!)
 
-        recycler_view_categories.layoutManager = LinearLayoutManager(context)
+        recycler_view_categories.layoutManager = LinearLayoutManager(activity!!.applicationContext)
         recycler_view_categories.addItemDecoration(dividerItemDecoration)
     }
 
@@ -75,35 +78,33 @@ class RecommendedMusicFragment : Fragment(), MusicController.AdapterCallbacks {
         musicController.setData(data)
     }
 
-    // TODO add temporary data
-    private fun dummyPlaylistData(): List<Category> {
-        val data = ArrayList<Category>()
+    private fun callCategoriesIndexService() {
+        if (AppUtils.isInternetConnected(activity!!.applicationContext)) {
+            val categoryService = ApiServiceFactory.create(CategoryServiceInterface::class.java)
 
-        val lorem = LoremIpsum.getInstance()
-
-        val images = arrayOf(
-                "https://hdwallsource.com/img/2013/19/anime-girls-2426.jpg",
-                "http://animefanatika.co.za/afwp/wp-content/uploads/2016/01/2015-cover.jpg",
-                "https://vignette.wikia.nocookie.net/date-a-live/images/e/e0/MA048001_1.png/revision/latest?cb=20130704113347",
-                "https://www.w3schools.com/w3css/img_fjords.jpg",
-                "http://www.ptahai.com/wp-content/uploads/2016/06/Best-Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg",
-                "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg"
-        )
-
-        val countImages = images.size
-
-        (0..20).forEach { i ->
-            val playlists = (0..5).map {
-                Playlist(it, lorem.getTitle(3, 5), "xxxurl", images[Random().nextInt(countImages)])
-            }
-
-            data.add(Category(i, "$i - ${lorem.getTitle(2, 4)}", lorem.getTitle(5, 8), playlists))
+            mCompositeDisposable.add(categoryService.recommendation()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { categories -> showCategories(categories) }
+            )
         }
+    }
 
-        return data
+    private fun showCategories(categories: List<Category>) {
+        updateMusicController(categories)
+
+        this.playMusicFragment.showMusicPlayer()
     }
 
     companion object {
         private val TAG = RecommendedMusicFragment::class.java.canonicalName
+
+        fun newInstance(playMusicFragment: PlayMusicFragment): RecommendedMusicFragment {
+            val recommendedMusicFragment = RecommendedMusicFragment()
+
+            recommendedMusicFragment.playMusicFragment = playMusicFragment
+
+            return recommendedMusicFragment
+        }
     }
 }
