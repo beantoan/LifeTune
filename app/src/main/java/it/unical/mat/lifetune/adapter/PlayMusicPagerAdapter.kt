4ca40cfe5edaa1a2bf1567d182a4d6725391a80f
Đậu Quốc Toản <beantoan@gmail.com>
2017-12-14 17:01:22 +0100

    Created BaseMusicFragment

--- app/src/main/java/it/unical/mat/lifetune/adapter/PlayMusicPagerAdapter.kt
+++ app/src/main/java/it/unical/mat/lifetune/adapter/PlayMusicPagerAdapter.kt
@@ -4,16 +4,21 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import it.unical.mat.lifetune.fragment.FavoriteMusicFragment
+import it.unical.mat.lifetune.fragment.PlayMusicFragment
 import it.unical.mat.lifetune.fragment.RecommendedMusicFragment
 
 /**
  * Created by beantoan on 11/17/17.
  */
-class PlayMusicPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class PlayMusicPagerAdapter(playMusicFragment: PlayMusicFragment, fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager) {
+
+    private val recommendationMusicFragment = RecommendedMusicFragment.newInstance(playMusicFragment)
+    private val favoriteMusicFragment = FavoriteMusicFragment.newInstance(playMusicFragment)
 
     override fun getItem(i: Int): Fragment = when (i) {
-        0 -> RecommendedMusicFragment()
-        else -> FavoriteMusicFragment()
+        0 -> recommendationMusicFragment
+        else -> favoriteMusicFragment
     }
 
     override fun getCount(): Int = TAB_COUNT
