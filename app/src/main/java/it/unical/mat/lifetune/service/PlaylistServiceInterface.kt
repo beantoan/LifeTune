06af1600712b087126263fc988f62fffd104bd85
Đậu Quốc Toản <beantoan@gmail.com>
2017-12-15 12:34:45 +0100

    Added api to get songs by playlist

--- app/src/main/java/it/unical/mat/lifetune/service/PlaylistServiceInterface.kt
+++ app/src/main/java/it/unical/mat/lifetune/service/PlaylistServiceInterface.kt
@@ -2,7 +2,9 @@ package it.unical.mat.lifetune.service
 
 import io.reactivex.Single
 import it.unical.mat.lifetune.entity.Playlist
+import it.unical.mat.lifetune.entity.Song
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * Created by beantoan on 5/14/16.
@@ -10,4 +12,7 @@ import retrofit2.http.GET
 interface PlaylistServiceInterface {
     @GET("playlists/favourite.json")
     fun favourite(): Single<List<Playlist>>
+
+    @GET("playlists/{id}/songs.json")
+    fun songs(@Path("id") songId: Int): Single<List<Song>>
 }
