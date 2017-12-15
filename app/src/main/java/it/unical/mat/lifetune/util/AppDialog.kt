4ea0e0e96c57708162b4d84a9b39232c19face0f
Đậu Quốc Toản <beantoan@gmail.com>
2017-12-15 11:01:01 +0100

    Re-Added SweetDialogAlert with fixing problem with Rotate3dAnimation error

---
+++ app/src/main/java/it/unical/mat/lifetune/util/AppDialog.kt
@@ -0,0 +1,98 @@
+package it.unical.mat.lifetune.util
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.util.Log
+import cn.pedant.SweetAlert.SweetAlertDialog
+import it.unical.mat.lifetune.R
+
+/**
+ * Created by beantoan on 8/27/16.
+ */
+object AppDialog {
+    private val TAG = "AppDialog"
+
+    private var alertDialog: SweetAlertDialog? = null
+    private var progressDialog: ProgressDialog? = null
+
+    fun hideAlert(activity: Activity) {
+        if (AppDialog.alertDialog != null) {
+            activity.runOnUiThread {
+                try {
+                    AppDialog.alertDialog!!.dismiss()
+                } catch (e: Exception) {
+                    Log.e(TAG, "hideAlert", e)
+                }
+            }
+        }
+    }
+
+    fun showAlert(titleId: Int, messageId: Int, dialogType: Int, activity: Activity) {
+        AppDialog.hideAlert(activity)
+
+        activity.runOnUiThread {
+            try {
+                AppDialog.alertDialog = SweetAlertDialog(activity, dialogType)
+                        .setTitleText(activity.getString(titleId))
+                        .setContentText(activity.getString(messageId))
+
+                AppDialog.alertDialog!!.show()
+            } catch (e: Exception) {
+                Log.e(TAG, "showAlert", e)
+            }
+        }
+    }
+
+    fun error(titleId: Int, messageId: Int, activity: Activity) {
+        AppDialog.showAlert(titleId, messageId, SweetAlertDialog.ERROR_TYPE, activity)
+    }
+
+    fun warning(titleId: Int, messageId: Int, activity: Activity) {
+        AppDialog.showAlert(titleId, messageId, SweetAlertDialog.WARNING_TYPE, activity)
+    }
+
+    fun success(titleId: Int, messageId: Int, activity: Activity) {
+        AppDialog.showAlert(titleId, messageId, SweetAlertDialog.SUCCESS_TYPE, activity)
+    }
+
+    private fun initProgressDialog(activity: Activity) {
+        AppDialog.progressDialog = ProgressDialog(activity)
+        AppDialog.progressDialog!!.setCancelable(false)
+        AppDialog.progressDialog!!.setCanceledOnTouchOutside(false)
+        AppDialog.progressDialog!!.setMessage(activity.getString(R.string.progress_dialog_waiting_message))
+    }
+
+    fun hideProgress(activity: Activity) {
+        if (AppDialog.progressDialog != null) {
+            activity.runOnUiThread {
+                try {
+                    AppDialog.progressDialog!!.dismiss()
+
+                } catch (e: Exception) {
+                    Log.e(TAG, "hideProgress", e)
+                }
+            }
+        }
+    }
+
+    fun showProgress(messageId: Int?, activity: Activity) {
+        AppDialog.initProgressDialog(activity)
+
+        AppDialog.hideProgress(activity)
+
+        if (AppDialog.progressDialog != null) {
+            if (messageId != null) {
+                AppDialog.progressDialog!!.setMessage(activity.getString(messageId))
+            }
+
+            activity.runOnUiThread {
+                try {
+                    AppDialog.progressDialog!!.show()
+
+                } catch (e: Exception) {
+                    Log.e(TAG, "showProgress", e)
+                }
+            }
+        }
+    }
+}
